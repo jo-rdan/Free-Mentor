d208edf778984a068bf049446c1a94c86a6fa222
Jo_Rdan250 <jordankayinamura@gmail.com>
2019-08-13 14:58:58 -0700

    feat(ui) mentor can accept/reject a request - create table of incomming requests from users - add button to accept or reject a specific user request [Finishes #167635360,#167635623]

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,8 +1,7 @@
 const menu_icon = document.querySelector('.menu');
 const side_menu = document.querySelector('.side-menu');
 const close_icon = document.querySelector('#close');
-// const action = document.querySelector('.actions');
-// const close_modal = document.querySelector('.close');
+
 
 menu_icon.addEventListener('click', (e) => {
   side_menu.style.display = 'block';
@@ -12,13 +11,12 @@ close_icon.addEventListener('click', (e) => {
   side_menu.style.display = 'none';
 });
 
-// action.addEventListener('click', openModal);
-// close_modal.addEventListener('click', closeModal)
-
 const closeModal = () => {
   document.querySelector('.pop-up').style.display = "none"
 }
 const openModal = () => {
   document.querySelector('.pop-up').style.display = "block"
 }
+
+
 
