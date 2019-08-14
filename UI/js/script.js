22decf03b727cafd8102cdf37899c72b20109e58
Jo_Rdan250 <jordankayinamura@gmail.com>
2019-08-14 11:49:43 -0700

    feat (ui) A user can review a mentor - add popup modal to allow user to give score an comments - add slider to slide through the score [Finishes #167636061]

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,7 +1,20 @@
 const menu_icon = document.querySelector('.menu');
 const side_menu = document.querySelector('.side-menu');
 const close_icon = document.querySelector('#close');
+const close = document.querySelector('.popup .close');
+const flag = document.querySelector('.basic-details .mentor-details i');
 
+const fetch = () => {
+  const get = document.querySelector('#get').value;
+  document.querySelector('#put').value = get;
+}
+
+const openFlag = () =>{
+  document.querySelector('.popup').style.display = "block"
+}
+const closeFlag = () =>{
+  document.querySelector('.popup').style.display = "none"
+}
 
 menu_icon.addEventListener('click', (e) => {
   side_menu.style.display = 'block';
